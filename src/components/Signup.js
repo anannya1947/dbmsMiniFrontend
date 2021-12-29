9f0e7589effcf9b6693bc1346d91dc1b696794c1
Anannya Mahapatra <anannya1947@gmail.com>
2021-12-29 08:59:07 +0530

    register-form elements

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,9 +1,28 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 export const Signup = () => {
+    const navigate = useNavigate()
     return (
         <div>
-            <h2>Signuppppppppppppp</h2>
+            <h1>Register</h1>
+            <div className='login-div'>
+                <input type="text" placeholder="name"/>
+                <br/>
+                <input type="text" placeholder="username"/>
+                <br/>
+                <input type="text" placeholder="email"/>
+                <br/>
+                <input type="text" placeholder="password"/>
+                <br/>
+                <input type="text" placeholder="re-enter password"/>
+                <br/>
+                <button className='btn' 
+                        onClick={() => {
+                            navigate("/search")
+                        }
+                }>Submit</button>
+                </div>
         </div>
     )
 }
