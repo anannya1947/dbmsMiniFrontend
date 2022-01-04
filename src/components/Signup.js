b8735ecef48bed1b7325002ae751ac680cf181c2
Anannya Mahapatra <anannya1947@gmail.com>
2022-01-04 22:05:33 +0530

    succesful register oage fe

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,25 +1,59 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
+import { useState, useEffect} from 'react'
+import Axios from 'axios'
 
 export const Signup = () => {
     const navigate = useNavigate()
+    const [name, setName] = useState('')
+    const [username, setUsername] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [age, setAge] = useState(0)
+
+    const submitRegister = () =>{
+        Axios.post('http://localhost:5001/db/register', {
+            name: name,
+            age : age,
+            username: username,
+            email: email,
+            password: password,
+          })
+          .then(()=>{
+            alert("successful insert")
+            navigate("/search")
+          })
+          .catch(err => console.log(err))
+    }
+
     return (
         <div>
             <h1>Register</h1>
             <div className='login-div'>
-                <input type="text" placeholder="name"/>
+                <input type="text" placeholder="name" onChange={(e)=>{
+                    setName(e.target.value)
+                }}/>
                 <br/>
-                <input type="text" placeholder="username"/>
+                <input type="number" placeholder="age" onChange={(e)=>{
+                    setAge(e.target.value)
+                }}/>
                 <br/>
-                <input type="text" placeholder="email"/>
+                <input type="text" placeholder="username" onChange={(e)=>{
+                    setUsername(e.target.value)
+                }}/>
                 <br/>
-                <input type="text" placeholder="password"/>
+                <input type="text" placeholder="email" onChange={(e)=>{
+                    setEmail(e.target.value)
+                }}/>
                 <br/>
-                <input type="text" placeholder="re-enter password"/>
+                <input type="text" placeholder="password" onChange={(e)=>{
+                    setPassword(e.target.value)
+                }}/>
                 <br/>
                 <button className='btn' 
                         onClick={() => {
-                            navigate("/search")
+                            // navigate("/search")
+                            submitRegister()
                         }
                 }>Submit</button>
                 </div>
