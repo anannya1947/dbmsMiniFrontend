e79b56fe1765d5981a59e292e46efd013eed52c8
Anannya Mahapatra <anannya1947@gmail.com>
2022-01-09 17:02:37 +0530

    profile page frontend

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,13 +1,59 @@
-import React from 'react'
-import { Header } from './Header'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
-export const Profile = ({ token }) => {
+export const Profile = ({ token, tk }) => {
+    useEffect(() => {
+        async function getProfile() {
+            try {
+                let res = await axios.get("http://localhost:5001/db/profile", {
+                    headers: {
+                        authorization: `bearer ${tk}`
+                    }
+                })
+                setResult(res.data[0])
+                console.log(res.data)
+                setFlag(true)
+            }
+            catch (error) {
+                console.log(error);
+            }
+
+        }
+        getProfile()
+    }, [])
+
+    const [result, setResult] = useState([])
+    const [flag, setFlag] = useState(false)
     return (
         <div>
-            {/* <h1>Profile </h1>
-            <p>{token.username}</p>
-            <p>{token.auth}</p>
-            <p>{token.id}</p> */}
+            {
+                flag ? <div>
+                    <p>User Name:
+                        {
+                            result.viewer_name
+                        }
+                    </p>
+                    <p>
+                        Date joined:
+                        {
+                            result.created_on
+                        }
+                    </p>
+                    <p>
+                        Number of wishlisted movies:
+                        {
+                            result.movies_wishlisted
+                        }
+                    </p>
+                    <p>
+                        Number of wishlisted shows:
+                        {
+                            result.shows_wishlisted
+                        }
+                    </p>
+
+                </div> : <></>
+            }
         </div>
     )
 }
