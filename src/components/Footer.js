558d0b365cf94918215bb56afca5799310e99fc5
Anannya Mahapatra <anannya1947@gmail.com>
2022-01-10 21:03:59 +0530

    tried fixing footer position

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import tmdbLogo from '../assets/tmdbLogo.jpg'
 
 function Footer() {
     return (
@@ -6,14 +7,21 @@ function Footer() {
         <div className="footer"
             style={{
                 position: "relative",
-                left: "0",
-                bottom: "0",
+                bottom: "-700px",
                 width: "100%",
                 backgroundColor: "black",
                 color: "lightgreen",
                 textAlign: "center",
                 height: "70px",
             }}>
+            <img
+                style={{
+                    marginTop: "10px",
+                    marginRight: "30px",
+                    height: "45px",
+                    width: "45px"
+                }}
+                src={tmdbLogo} alt="tmdb Logo"></img>
             Copyright © 2022 by Tempus
         </div >
 
