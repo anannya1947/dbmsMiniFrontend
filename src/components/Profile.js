98a0adf5d1439f5e50bb694b4a276a3003f4292c
harsha0571 <harsha90571@gmail.com>
2022-01-10 12:31:46 +0530

    added recom to profile page

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
-
+import DetailRecomm from './DetailRecomm'
 export const Profile = ({ token, tk }) => {
     useEffect(() => {
         async function getProfile() {
@@ -20,38 +20,72 @@ export const Profile = ({ token, tk }) => {
 
         }
         getProfile()
+        async function getRecomm() {
+            try {
+                let res = await axios.get("http://localhost:5001/db/recomm", {
+                    headers: {
+                        authorization: `bearer ${tk}`
+                    }
+                })
+                setRecomm(res.data)
+                console.log(res.data)
+
+
+            }
+            catch (error) {
+                console.log(error);
+            }
+
+        }
+        getRecomm()
+
     }, [])
 
     const [result, setResult] = useState([])
+    const [recomm, setRecomm] = useState([])
     const [flag, setFlag] = useState(false)
     return (
         <div>
             {
                 flag ? <div>
-                    <p>User Name:
+                    <div>
+                        <p>User Name:
+                            {
+                                result.viewer_name
+                            }
+                        </p>
+                        <p>
+                            Date joined:
+                            {
+                                result.created_on
+                            }
+                        </p>
+                        <p>
+                            Number of wishlisted movies:
+                            {
+                                result.movies_wishlisted
+                            }
+                        </p>
+                        <p>
+                            Number of wishlisted shows:
+                            {
+                                result.shows_wishlisted
+                            }
+                        </p>
+                    </div>
+                    <div>
                         {
-                            result.viewer_name
-                        }
-                    </p>
-                    <p>
-                        Date joined:
-                        {
-                            result.created_on
-                        }
-                    </p>
-                    <p>
-                        Number of wishlisted movies:
-                        {
-                            result.movies_wishlisted
-                        }
-                    </p>
-                    <p>
-                        Number of wishlisted shows:
-                        {
-                            result.shows_wishlisted
-                        }
-                    </p>
+                            recomm.map(details => {
+                                if (details.media_type === "tv" || details.media_type === "movie")
+                                    return (
 
+                                        <DetailRecomm key={details.entry_id} props={details} btn={"true"} token={token} />
+
+                                    )
+
+                            })
+                        }
+                    </div>
                 </div> : <></>
             }
         </div>
