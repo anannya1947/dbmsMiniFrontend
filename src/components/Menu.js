4d21b9095e9109c5ea5e7896080507b018201d76
harsha0571 <harsha90571@gmail.com>
2022-01-17 15:17:26 +0530

    spaces btw btn

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -4,6 +4,7 @@ function Menu({ fn }) {
     return (
         <div>
             <button className='btn' onClick={() => fn(false, "watched")}>Watched</button>
+            <span>  </span>
             <button className='btn' onClick={() => fn(false, "watchlist")}>Watchlist</button>
         </div>
     )
